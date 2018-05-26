a898bae09875d5100a7e755ea4147ebccf89f6b9
Abner Carleto <abner.carleto@ymail.com>
2018-05-26 15:37:00 -0300

    add morgan to log requests

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import morgan from 'morgan'
 
 const app = express();
+
+app.use(morgan('[:date[clf]] ":method :url" :status :response-time ms'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
